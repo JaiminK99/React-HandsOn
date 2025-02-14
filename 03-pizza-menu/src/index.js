712c6ca03caf480af01cb28e650c6d3492c87b63
JaiminK99 <koriyajaimin1999@gmail.com>
2025-02-14 23:55:22 +0530

    Added strict mode

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>hello React!</h1>;
+  return <h1>Hello React!</h1>;
 }
 
+// React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-root.render(<App />);
+// Before Reacct v18
+// React.render(<App />, document.getElementById("root"));
