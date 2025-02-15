5092c1673f0f46554bf11d2336074c49971d163b
JaiminK99 <koriyajaimin1999@gmail.com>
2025-02-15 23:15:53 +0530

    Conditional operating with ternary operator

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -76,12 +76,14 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzaData.map((p) => (
             <Pizza pizzaObj={p} key={p.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
       )}
     </main>
   );
@@ -111,11 +113,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open till {closehour}:00. Come visit us or order online. </p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closehour}:00.
+        </p>
       )}
     </footer>
   );
