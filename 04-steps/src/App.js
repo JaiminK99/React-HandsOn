459d2345e7fa26e9d3ac0f73bd15cb71f99ec1f7
JaiminK99 <koriyajaimin1999@gmail.com>
2025-02-22 20:07:54 +0530

    Added state

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,18 +7,23 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 2;
+  const [step, setStep] = useState(1);
+  // const step = 2;
 
-  const handlePrevious = () => {};
+  const handlePrevious = () => {
+    if (step > 1) setStep(step - 1);
+  };
 
-  const handleNext = () => {};
+  const handleNext = () => {
+    if (step < 3) setStep(step + 1);
+  };
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
 
       <p className="message">
