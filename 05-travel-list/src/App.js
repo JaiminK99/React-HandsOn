246fc0a6e736c3644815a2fa978bc05d0a754583
JaiminK99 <koriyajaimin1999@gmail.com>
2025-02-22 23:19:28 +0530

    Initial commit of travel application

---
+++ 05-travel-list/src/App.js
@@ -0,0 +1,32 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ far Away 🧳</h1>;
+}
+
+function Form() {
+  return <div className="add-form">What do you need for your 😍 trip?</div>;
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        💼 ✈️ 🚀 You have X items on your list, and you already packed X (X%).
+      </em>
+    </footer>
+  );
+}
