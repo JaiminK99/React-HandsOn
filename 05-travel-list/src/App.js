7afe47dff53c6598a343f1a8ffcd9d111c927db0
JaiminK99 <koriyajaimin1999@gmail.com>
2025-02-27 00:13:12 +0530

    Added functionality of removing items

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,11 +1,5 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Charger", quantity: 1, packed: true },
-  { id: 3, description: "Socks", quantity: 12, packed: false },
-];
-
 export default function App() {
   // Lifting up the state
   const [items, setItems] = useState([]);
@@ -14,11 +8,15 @@ export default function App() {
     setItems((items) => [...items, item]);
   };
 
+  const handleDeleteItem = (id) => {
+    setItems((items) => items.filter((item) => item.id !== id));
+  };
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItems={handleDeleteItem} />
       <Stats />
     </div>
   );
@@ -48,6 +46,7 @@ function Form({ onAddItems }) {
 
   // 3. Update state variable using onChange event and set state variale to current value of the element
 
+  // Part of "Lifting up state"
   // const handleAddItems = (item) => {
   //   setItems((items) => [...items, item]);
   // };
@@ -94,25 +93,25 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItems }) {
   return (
     <div className="list">
       <ul className="list">
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} onDeleteItems={onDeleteItems} key={item.id} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItems }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItems(item.id)}>❌</button>
     </li>
   );
 }
