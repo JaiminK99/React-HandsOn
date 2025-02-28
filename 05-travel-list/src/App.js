ca7be4d2da309375c192d1ea905be4649f36afb0
JaiminK99 <koriyajaimin1999@gmail.com>
2025-02-28 10:29:58 +0530

    Implimented functionality of sorting list and clearig list

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -20,6 +20,14 @@ export default function App() {
     );
   };
 
+  const handleClearList = () => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
+  };
+
   return (
     <div className="app">
       <Logo />
@@ -28,6 +36,7 @@ export default function App() {
         items={items}
         onDeleteItems={handleDeleteItem}
         onToggleItem={handleToggleItem}
+        onHandleClearList={handleClearList}
       />
       <Stats items={items} />
     </div>
@@ -105,11 +114,30 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items, onDeleteItems, onToggleItem }) {
+function PackingList({
+  items,
+  onDeleteItems,
+  onToggleItem,
+  onHandleClearList,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul className="list">
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDeleteItems={onDeleteItems}
@@ -118,6 +146,14 @@ function PackingList({ items, onDeleteItems, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onHandleClearList}>Clear List</button>
+      </div>
     </div>
   );
 }
