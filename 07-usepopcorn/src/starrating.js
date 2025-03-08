43c1a74d2e4bc80be222ed2e9f3ad90c44933fb1
JaiminK99 <koriyajaimin1999@gmail.com>
2025-03-08 23:55:59 +0530

    Initialized Proptypes for type checking

--- 07-usepopcorn/src/starrating.js
+++ 07-usepopcorn/src/starrating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -9,6 +10,16 @@ const containerStyle = {
 const starContainerStyle = {
   display: "flex",
   //   gap: "4px",
+};
+
+StarRating.prototype = {
+  maxRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  className: PropTypes.string,
+  messages: PropTypes.array,
+  defaultRating: PropTypes.number,
+  onSetMovieRating: PropTypes.func,
 };
 
 export default function StarRating({
