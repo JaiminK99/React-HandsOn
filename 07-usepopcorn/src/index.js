29d8866b6fbaac5ace6dd620454d80a83ab9dbb1
JaiminK99 <koriyajaimin1999@gmail.com>
2025-03-08 23:43:44 +0530

    Improved reusability of component with prop as component API

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 import App from "./App";
 import StarRating from "./starrating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        color="blue"
+        maxRating={10}
+        onSetMovieRating={setMovieRating}
+      />
+      <p>This movie was rated {movieRating} rating.</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating color="red" size={24} className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
