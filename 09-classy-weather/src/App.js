f46ef83b0b465bbb62f018a2aca79630e8008dc8
JaiminK99 <koriyajaimin1999@gmail.com>
2025-06-08 19:58:50 +0530

    Clean up and first class based component

--- 09-classy-weather/src/App.js
+++ 09-classy-weather/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends React.Component {
+  //Class components can't use states.
+  //To use state in class components we need to call Constructor method.Which receives props
+  //THis method gets called everytime a new object instanstiated from class
+  constructor(props) {
+    // Constructor uses Super method to call parent constructor to craete state
+    super(props);
+
+    //To inisialise the state
+    //For every state we need , we have to create new property in state object
+    this.state = { count: 5 };
+  }
+
+  render() {
+    return (
+      <div>
+        <button>-</button>
+        <span>{this.state.count}</span>
+        <button>+</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Counter;
