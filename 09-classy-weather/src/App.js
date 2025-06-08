f2867314447932ee126173824302b7611b699a7b
JaiminK99 <koriyajaimin1999@gmail.com>
2025-06-08 20:18:02 +0530

    Event handlers and this keyword binding in Class components

--- 09-classy-weather/src/App.js
+++ 09-classy-weather/src/App.js
@@ -11,14 +11,36 @@ class Counter extends React.Component {
     //To inisialise the state
     //For every state we need , we have to create new property in state object
     this.state = { count: 5 };
+
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  //handler functions are defined as calss methods
+  handleDecrement() {
+    this.setState((curState) => {
+      return { count: curState.count - 1 };
+    });
+  }
+
+  handleIncrement() {
+    this.setState((curState) => {
+      return { count: curState.count + 1 };
+    });
   }
 
   render() {
+    const date = new Date("june 21 2027");
+    date.setDate(date.getDate() + this.state.count);
+
     return (
       <div>
-        <button>-</button>
-        <span>{this.state.count}</span>
-        <button>+</button>
+        <button onClick={this.handleDecrement}>-</button>{" "}
+        {/*All event handlers called in JSX loses their binding to this keywork. Bcs JS will create a copy of callback function which is not bound to any object*/}
+        <span>
+          {date.toDateString()} [{this.state.count}]
+        </span>
+        <button onClick={this.handleIncrement}>+</button>
       </div>
     );
   }
