16dee85b154a4eab4863c7c67835700cde25d235
JaiminK99 <koriyajaimin1999@gmail.com>
2025-06-16 23:32:29 +0530

    Class based component life cycle

--- 09-classy-weather/src/App.js
+++ 09-classy-weather/src/App.js
@@ -39,7 +39,7 @@ class App extends React.Component {
 
   //Class fields
   state = {
-    location: "lisbon",
+    location: "",
     isLoading: false,
     displayLocation: "",
     weather: {},
@@ -55,6 +55,7 @@ class App extends React.Component {
 
   // For definfing class fields we can use arrow functions
   fetchWeather = async () => {
+    if (this.state.location.length < 2) return this.setState({ weather: {} });
     try {
       this.setState({ isLoading: true });
 
@@ -80,13 +81,32 @@ class App extends React.Component {
       const weatherData = await weatherRes.json();
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.err(err);
+      console.error(err);
     } finally {
       this.setState({ isLoading: false });
     }
   };
 
   setLocation = (e) => this.setState({ location: e.target.value });
+
+  // Component life cycle method: Immediatly called after rendering.
+  // Not same as useEffect hook (useEffect with empty dependency array)
+  // Only runs when component mounts and not at re-renders
+  componentDidMount() {
+    // this.fetchWeather();
+    this.setState({ location: localStorage.getItem("location") || "" });
+  }
+
+  // Component life cycle method: Called only on re-renders
+  // React gives access to previous state and previous props
+  // similar useEffect with some variable with dependency array.
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+
+      localStorage.setItem("location", this.state.location);
+    }
+  }
 
   render() {
     return (
@@ -96,7 +116,6 @@ class App extends React.Component {
           location={this.state.location}
           onChangeLocation={this.setLocation}
         />
-        <button onClick={this.fetchWeather}>Get Weather</button>
         {this.state.isLoading && <p className="loader">Loading...</p>}
         {this.state.weather.weathercode && (
           <Weather
@@ -127,6 +146,12 @@ class Input extends React.Component {
 }
 
 class Weather extends React.Component {
+  // Component life cycle method: Runs after the component unmounts.
+  // SImilar to returning a cleanup function in useEffect
+  componentWillUnmount() {
+    console.log("Weather will unmount.");
+  }
+
   render() {
     const {
       temperature_2m_max: max,
