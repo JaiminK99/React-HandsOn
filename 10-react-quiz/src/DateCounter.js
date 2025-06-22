c14360530449c4c5926a19d02a6a12f4a2016cb1
JaiminK99 <koriyajaimin1999@gmail.com>
2025-06-22 19:00:52 +0530

    useReducer hook for both 'count' and 'step' states

--- 10-react-quiz/src/DateCounter.js
+++ 10-react-quiz/src/DateCounter.js
@@ -2,11 +2,26 @@ import { useReducer, useState } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
+
+  switch (action.type) {
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "setCount":
+      return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    default:
+      throw new Error("UNknown Error");
+  }
+
   // return state + action;
-  if (action.type === "dec") return state - 1;
+  // if (action.type === "dec") return state - 1;
+
   // If we are using direct value of action then no need of payload property in dispatch
-  if (action.type === "inc") return state + 1;
-  if (action.type === "setCount") return action.payload;
+  // if (action.type === "inc") return state + 1;
+  // if (action.type === "setCount") return action.payload;
 }
 
 function DateCounter() {
@@ -14,9 +29,15 @@ function DateCounter() {
   // useReducer works with reducer function.
   // It is a pure function that always takes in previous state and so called action as an argument and will return next state.
   // const [count, setCount] = useState(0);
-  const [count, dispatch] = useReducer(reducer, 0);
+  // const [step, setStep] = useState(1);
 
-  const [step, setStep] = useState(1);
+  // useReducer hook is used when we have more than one complex state to manage.
+  // state will be an object and not a single value
+  const initialState = { count: 0, step: 1 };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -40,12 +61,13 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
+    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
   };
 
   return (
