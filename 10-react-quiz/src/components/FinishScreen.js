7bf7f26eac72d97b2f2ea5625a172d73bd957558
JaiminK99 <koriyajaimin1999@gmail.com>
2025-07-27 23:52:15 +0530

    Added quiz timer for time limit

--- 10-react-quiz/src/components/FinishScreen.js
+++ 10-react-quiz/src/components/FinishScreen.js
@@ -1,10 +1,18 @@
 function FinishScreen({ points, maxPoints, highscore, dispatch }) {
   const percentage = (points / maxPoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🥇";
+  if (percentage >= 80 && percentage < 100) emoji = "🎉";
+  if (percentage >= 50 && percentage < 80) emoji = "🙃";
+  if (percentage > 0 && percentage < 50) emoji = "🤨";
+  if (percentage === 0) emoji = "🤦‍♂️";
+
   return (
     <>
       <p className="result">
-        You scorerd <strong>{points}</strong> out of {maxPoints} (
-        {Math.ceil(percentage)} %)
+        <span>{emoji}</span>You scorerd <strong>{points}</strong> out of{" "}
+        {maxPoints} ({Math.ceil(percentage)} %)
       </p>
       <p className="highscore">(Highscore: {highscore} points)</p>
       <button
