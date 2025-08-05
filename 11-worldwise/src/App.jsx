d5af911e80616a8819426a38e76d8290ae6d5cd1
JaiminK99 <koriyajaimin1999@gmail.com>
2025-08-06 00:13:30 +0530

    Fetched the data from API and displayed Cities

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -5,9 +5,32 @@ import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import Applayout from "./pages/Applayout";
 import Login from "./pages/Login";
-import City from "./component/City";
+import CityList from "./component/CityList";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const BASE_URL = "http://localhost:8000";
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -17,9 +40,15 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="app" element={<Applayout />}>
           {/*Below are Nested Route :  because it is a route inside a app layout rout */}
-          <Route index element={<City />} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           {/*index rout : When if neither of the nested path matches then this index path will be displayed */}
-          <Route path="cities" element={<City />}></Route>
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          ></Route>
           <Route path="countries" element={<p>List of countries</p>}></Route>
           <Route path="form" element={<p>Form</p>}></Route>
         </Route>
