63f2ca2fe82bab0a3dae2b3fc6aa8979297347fe
JaiminK99 <koriyajaimin1999@gmail.com>
2025-08-05 22:52:25 +0530

    Nested Routes and Index routes

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -5,6 +5,7 @@ import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import Applayout from "./pages/Applayout";
 import Login from "./pages/Login";
+import City from "./component/City";
 
 function App() {
   return (
@@ -14,7 +15,14 @@ function App() {
         <Route path="products" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<Applayout />} />
+        <Route path="app" element={<Applayout />}>
+          {/*Below are Nested Route :  because it is a route inside a app layout rout */}
+          <Route index element={<City />} />
+          {/*index rout : When if neither of the nested path matches then this index path will be displayed */}
+          <Route path="cities" element={<City />}></Route>
+          <Route path="countries" element={<p>List of countries</p>}></Route>
+          <Route path="form" element={<p>Form</p>}></Route>
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
