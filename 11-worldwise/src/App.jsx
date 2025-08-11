b4eaf19df884f62a3567b5406dd59629da11377c
JaiminK99 <koriyajaimin1999@gmail.com>
2025-08-11 23:54:54 +0530

    Navigate component use in declarative way to define rout for index component

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -43,11 +43,18 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="app" element={<Applayout />}>
           {/*Below are Nested Route :  because it is a route inside a app layout rout */}
+          {/*index rout : When if neither of the nested path matches then this index path will be displayed */}
+          {/* 1st method : using this method does not go to destination with correct path. So we will have to take each station one by one 
+          ex: below path goes to cities with path : http://localhost:5173/app (Will give error if wwe directly try to access cities)
           <Route
             index
             element={<CityList cities={cities} isLoading={isLoading} />}
-          />
-          {/*index rout : When if neither of the nested path matches then this index path will be displayed */}
+          /> */}
+          {/* 2nd Method : using navigate component of router dom it points to the correct chekpoints and reaches destination
+          as soon as index route is hit it will redirect to cities route 
+          ex: below method goes to the cities with path : http://localhost:5173/app/cities*/}
+          <Route index element={<Navigate replace to="cities" />} />
+          {/* Replace keyword to go back on the index path : Will replace current element in history stack*/}
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
