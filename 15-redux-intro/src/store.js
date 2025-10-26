9342eb984d4c17751c48691f40e47991cd1aee84
JaiminK99 <koriyajaimin1999@gmail.com>
2025-10-26 19:58:52 +0530

    Adding action creator functions to automate action types

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,5 +1,6 @@
 // Pure redux code without react intgration
 
+import { type } from "@testing-library/user-event/dist/type";
 import { createStore } from "redux";
 
 const initialState = {
@@ -40,17 +41,51 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 500 });
-store.dispatch({ type: "account/withdraw", payload: 200 });
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+// console.log(store.getState());
+
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "Buy a car" },
+// });
+
+// console.log(store.getState());
+
+// store.dispatch({ type: "account/payLoan" });
+
+// console.log(store.getState());
+
+// Action creator functions
+// It is a JS function. Not a redux feature or necessary, But helpful convention to automate action creation
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
 console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car" },
-});
-
+store.dispatch(withdraw(200));
 console.log(store.getState());
 
-store.dispatch({ type: "account/payLoan" });
+store.dispatch(requestLoan(1000, "Buy a bicycle"));
+console.log(store.getState());
 
+store.dispatch(payLoan());
 console.log(store.getState());
