b9878cd95f520f038fd71a700db9d6de0d89184c
JaiminK99 <koriyajaimin1999@gmail.com>
2025-10-30 23:52:20 +0530

    Creating a store with redux-toolkit

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,33 +1,9 @@
-// Pure redux code without react intgration
-
-// To use middleware in redux need to follow 3 steps
-// 1> Install redux-thunk package
-// 2> Apply middleware to the store
-// 3> use middleware in our action creator functions
-
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
-import { thunk } from "redux-thunk";
-
-// To use redux-devtools need to follow 3 steps
-// 1> Install chrome browser extension
-// 2> Install npm package (npm i @redux-devtools/extension)
-// 3> import composewithdevtools function
-import { composeWithDevTools } from "@redux-devtools/extension";
-
-// We can't pass two reducers in one store like parameters.
-// Need to create a root reducer by combining all teh reducers.
-
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: { account: accountReducer, customer: customerReducer },
 });
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-); // Applied middleware to the store , Apply redux-devtools-extension with composeWithDevTools
 
 export default store;
