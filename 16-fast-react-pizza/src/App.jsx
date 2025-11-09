cb69014f99b3d16d2a37dc160fa626d9d26a7431
JaiminK99 <koriyajaimin1999@gmail.com>
2025-11-09 15:43:58 +0530

    Implmenting children routes inside applayout using react-router

--- 16-fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/src/App.jsx
@@ -4,25 +4,31 @@ import Menu from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
+import Applayout from "./ui/Applayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+    element: <Applayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      { path: "/order/:orderIsd", element: <Order /> },
+    ],
   },
-  {
-    path: "menu/",
-    element: <Menu />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  {
-    path: "order/new",
-    element: <CreateOrder />,
-  },
-  { path: "order/:id", element: <Order /> },
 ]);
 
 export default function App() {
